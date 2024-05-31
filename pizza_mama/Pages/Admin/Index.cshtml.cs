5ecdff6cff424f42bfaa4e38db5f39d25bc4fb92
Yvan19 <dixneufcode@gmail.com>
2024-05-31 09:55:56 +0200

    Option de déconnection

--- pizza_mama/Pages/Admin/Index.cshtml.cs
+++ pizza_mama/Pages/Admin/Index.cshtml.cs
@@ -37,5 +37,11 @@ namespace pizza_mama.Pages.Admin
             return Page();
         }
 
+        public async Task<IActionResult> OnGetLogout()
+        {
+            await HttpContext.SignOutAsync();
+            return Redirect("/Admin");
+        }
+
     }
 }
